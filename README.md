937a4dbf4648e8a85b054d938b5c33bd360169d9
Sertac Ozercan <sozercan@gmail.com>
2016-07-15 17:16:22 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # slack-typobot
-Slack bot that corrects typos
+
+Slack bot that corrects typos ending with an asterisk
+
+## Screenshot
+
+![](http://i.imgur.com/1YWEUOS.gif)
+
+## Install
+
+Create a file called `conf.json` and put your API token in it:
+
+    {
+        "token": "PUT-TOKEN-HERE"
+    }
+
+Install dependencies:
+
+    npm install
+
+`cd` into the directory containing `app` to run:
+
+    node app.js
+
+## Dependencies
+
+The `package.json` exists but this package isn't published.
+
+Thanks to @Decagon/star-correct for https://github.com/Decagon/star.js
---
+++ app.js
@@ -0,0 +1,58 @@
+var fs     = require('fs');
+var config = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
+var token  = process.env.SLACK_API_TOKEN || config.token;
+var starjs = require('star-correct');
+
+var RTM_EVENTS        = require('@slack/client').RTM_EVENTS;
+var RTM_CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS.RTM;
+var CLIENT_EVENTS 	  = require('@slack/client').CLIENT_EVENTS;
+
+var RtmClient       = require('@slack/client').RtmClient;
+var rtm             = new RtmClient(token, {logLevel: 'error', dataStore: mds});
+var MemoryDataStore = require('@slack/client').MemoryDataStore;
+var mds             = new MemoryDataStore();
+
+var channel;
+var connected = false;
+var lastMessage;
+var userId;
+
+/**
+ * Open connection to send messages
+ */
+rtm.on(RTM_CLIENT_EVENTS.RTM_CONNECTION_OPENED, function() {
+	connected = true;
+});
+
+/**
+ * Retrieving user ID
+ */
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, function (rtmStartData) {
+	userId = rtmStartData.self.id;
+});
+
+/**
+ * Listen to `message` events
+ */
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+	if (connected) {
+		if(message.user == userId) {
+			if (message.text && (message.text).endsWith('*') === false) {
+				lastMessage = message;
+			}
+
+			if (message.text && (message.text).endsWith('*')) {
+				var correctedMsg = starjs.correct([lastMessage.text, message.text]);
+				if(correctedMsg) {
+					message.text = correctedMsg;
+
+					rtm.updateMessage(message, function () {
+						rtm.deleteMessage(lastMessage);
+					});
+				}
+			}
+		}
+	}
+});
+
+rtm.start();
